a69afbe177c29de1d5757e0793d323462cb17161
adenehemia09 <83700447+adenehemia09@users.noreply.github.com>
2022-06-12 16:39:42 +0700

    Day 1

---
+++ lib/widget/produck_overview_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductOverviewPage extends StatelessWidget {
+  const ProductOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
